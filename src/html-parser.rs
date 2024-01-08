d2150ba133eb97311d32cdaba20fb84a9b03b101
GuiltedRose <guiltedrose@proton.me>
2024-01-08 02:14:18 -0500

    Started writing parsing algorithm

--- src/html-parser.rs
+++ src/html-parser.rs
@@ -5,12 +5,24 @@ struct Element {
 }
 
 fn parse_html(html: &str) -> Element {
-    let root_element = Element {
-        tag_name: "html".to_string(),
-        attributes: HashMap::new(),
-        children: vec! [],
+    let mut root_element: Option<Element> = None;
+    let mut current_element: Option<Element> = None;
+    for token in html.split_whitespace(){
+        match token{
+            "<html>" => {
+                let element = Element {
+                    tag_name: "html".to_string(),
+                    attributes: HashMap::new(),
+                    children: Vec::new(),
+                };
+                root_element = Some(element);
+                current_element = root_element.clone();
+            }
+            "</html>" => {
+                current_element = None;
+            }
+        }
+    }
 
-    };
-
-    root_element
+    root_element.unwrap()
 }
